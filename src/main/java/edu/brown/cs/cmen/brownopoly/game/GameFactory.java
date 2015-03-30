f3031456413984d5fc0db9ad81f86e12e3cb59fb
Nickie Pucel <nicholas_pucel@brown.edu>
2015-03-30 11:47:08 -0400

    Added GameFactory

---
+++ src/main/java/edu/brown/cs/cmen/brownopoly/game/GameFactory.java
@@ -0,0 +1,31 @@
+package edu.brown.cs.cmen.brownopoly.game;
+
+/**
+ * 
+ * @author nipucel
+ *
+ */
+/* friendly */final class GameFactory {
+
+  /**
+   * Creates a Game and its aspects: Players, Properties, BoardSquares, Referee,
+   * etc.
+   * 
+   * Used when the user decides to start a new game
+   * 
+   * @return the newly created Game
+   */
+  public Game createGame(GameSettings settings) {
+    // Create Board
+
+    // Create Players
+
+    // Create Referee
+    // new Referee(board, players)
+
+    // Create Game
+    // new Game(Referee, ?board?)
+
+    return null;
+  }
+}
