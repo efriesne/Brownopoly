5f47cf334d33ebd8042d74bb12a73b10907f11d8
Marley Rafson <marley_rafson@brown.edu>
2015-04-01 17:29:13 -0400

    Trying to get a dummy player working

--- src/main/java/edu/brown/cs/cmen/brownopoly/game/Main.java
+++ src/main/java/edu/brown/cs/cmen/brownopoly/game/Main.java
@@ -43,91 +43,92 @@ public class Main {
   }
 
   private void run() {
-    runSparkServer();
+    new GUIRunner();
+//    runSparkServer();
   }
 
-  private static FreeMarkerEngine createEngine() {
-    Configuration config = new Configuration();
-    File templates = new File("src/main/resources/spark/template/freemarker");
-    try {
-      config.setDirectoryForTemplateLoading(templates);
-    } catch (IOException ioe) {
-      System.out.printf("ERROR: Unable use %s for template loading.%n",
-          templates);
-      System.exit(1);
-    }
-    return new FreeMarkerEngine(config);
-  }
-
-  /**
-   * Runs the spark server.
-   *
-   * @author mprafson
-   *
-   */
-  private void runSparkServer() {
-    Spark.externalStaticFileLocation("src/main/resources/static");
-    Spark.exception(Exception.class, new ExceptionPrinter());
-    // Spark.setPort(ip.getPort());
-
-    FreeMarkerEngine freeMarker = createEngine();
-
-    // Setup Spark Routes
-    Spark.get("/monopoly", new FrontHandler(), freeMarker);
-
-    // Do we need this?
-    /*
-     * Allows for the connection to the DB to be closed. Waits for the user to
-     * hit "enter" or "CTRL-D"
-     */
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(
-        System.in, "UTF-8"))) {
-      String line = null;
-      while ((line = br.readLine()) != null) {
-        if (line.equals("")) {
-          break;
-        }
-      }
-    } catch (UnsupportedEncodingException e) {
-      System.err.println("ERROR: Main, runSparkServer: Not UTF-8");
-    } catch (IOException e) {
-      System.err.println("ERROR: Main, runSparkServer: reader failure.");
-    } finally {
-      Spark.stop();
-    }
-  }
-
-  /**
-   * Sets up the opening page.
-   *
-   * @author mprafson
-   *
-   */
-  private static class FrontHandler implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of("title", "Monopoly");
-      return new ModelAndView(variables, "monopoly.ftl");
-    }
-  }
-
-  /**
-   * Handles exceptions. Sends them to the server.
-   *
-   * @author mprafson
-   *
-   */
-  private static class ExceptionPrinter implements ExceptionHandler {
-    @Override
-    public void handle(Exception e, Request req, Response res) {
-      res.status(STATUS);
-      StringWriter stacktrace = new StringWriter();
-      try (PrintWriter pw = new PrintWriter(stacktrace)) {
-        pw.println("<pre>");
-        e.printStackTrace(pw);
-        pw.println("</pre>");
-      }
-      res.body(stacktrace.toString());
-    }
-  }
+//  private static FreeMarkerEngine createEngine() {
+//    Configuration config = new Configuration();
+//    File templates = new File("src/main/resources/spark/template/freemarker");
+//    try {
+//      config.setDirectoryForTemplateLoading(templates);
+//    } catch (IOException ioe) {
+//      System.out.printf("ERROR: Unable use %s for template loading.%n",
+//          templates);
+//      System.exit(1);
+//    }
+//    return new FreeMarkerEngine(config);
+//  }
+//
+//  /**
+//   * Runs the spark server.
+//   *
+//   * @author mprafson
+//   *
+//   */
+//  private void runSparkServer() {
+//    Spark.externalStaticFileLocation("src/main/resources/static");
+//    Spark.exception(Exception.class, new ExceptionPrinter());
+//    // Spark.setPort(ip.getPort());
+//
+//    FreeMarkerEngine freeMarker = createEngine();
+//
+//    // Setup Spark Routes
+//    Spark.get("/monopoly", new FrontHandler(), freeMarker);
+//
+//    // Do we need this?
+//    /*
+//     * Allows for the connection to the DB to be closed. Waits for the user to
+//     * hit "enter" or "CTRL-D"
+//     */
+//    try (BufferedReader br = new BufferedReader(new InputStreamReader(
+//        System.in, "UTF-8"))) {
+//      String line = null;
+//      while ((line = br.readLine()) != null) {
+//        if (line.equals("")) {
+//          break;
+//        }
+//      }
+//    } catch (UnsupportedEncodingException e) {
+//      System.err.println("ERROR: Main, runSparkServer: Not UTF-8");
+//    } catch (IOException e) {
+//      System.err.println("ERROR: Main, runSparkServer: reader failure.");
+//    } finally {
+//      Spark.stop();
+//    }
+//  }
+//
+//  /**
+//   * Sets up the opening page.
+//   *
+//   * @author mprafson
+//   *
+//   */
+//  private static class FrontHandler implements TemplateViewRoute {
+//    @Override
+//    public ModelAndView handle(Request req, Response res) {
+//      Map<String, Object> variables = ImmutableMap.of("title", "Monopoly");
+//      return new ModelAndView(variables, "monopoly.ftl");
+//    }
+//  }
+//
+//  /**
+//   * Handles exceptions. Sends them to the server.
+//   *
+//   * @author mprafson
+//   *
+//   */
+//  private static class ExceptionPrinter implements ExceptionHandler {
+//    @Override
+//    public void handle(Exception e, Request req, Response res) {
+//      res.status(STATUS);
+//      StringWriter stacktrace = new StringWriter();
+//      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+//        pw.println("<pre>");
+//        e.printStackTrace(pw);
+//        pw.println("</pre>");
+//      }
+//      res.body(stacktrace.toString());
+//    }
+//  }
 }
