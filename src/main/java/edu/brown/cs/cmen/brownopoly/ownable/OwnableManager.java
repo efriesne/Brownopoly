a05e4c1b3c113353f1b9d8c03f3c8cb544ad6cbf
Nickie Pucel <nicholas_pucel@brown.edu>
2015-04-01 17:28:00 -0400

    Added OwnableManager, OwnableFactory, Bank

---
+++ src/main/java/edu/brown/cs/cmen/brownopoly/ownable/OwnableManager.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.cmen.brownopoly.ownable;
+
+public class OwnableManager {
+
+  public static Property getProperty(int i) {
+    return null;
+  }
+
+  public static Property getRandomProperty() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
