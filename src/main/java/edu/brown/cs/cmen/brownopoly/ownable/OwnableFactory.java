704605ff16835b3edcc42b3044ac91316f35a5b9
Nickie Pucel <nicholas_pucel@brown.edu>
2015-04-16 15:05:29 -0400

    began work on OwnableManager and OwnableFactory

--- src/main/java/edu/brown/cs/cmen/brownopoly/ownable/OwnableFactory.java
+++ src/main/java/edu/brown/cs/cmen/brownopoly/ownable/OwnableFactory.java
@@ -1,5 +1,19 @@
 package edu.brown.cs.cmen.brownopoly.ownable;
 
+import java.util.Map;
+
 class OwnableFactory {
+
+  Map<Integer, Property> buildProperties() {
+    return null;
+  }
+
+  Map<Integer, Railroad> buildRailroads() {
+    return null;
+  }
+
+  Map<Integer, Property> buildUtilities() {
+    return null;
+  }
 
 }
--- src/main/java/edu/brown/cs/cmen/brownopoly/ownable/OwnableManager.java
+++ src/main/java/edu/brown/cs/cmen/brownopoly/ownable/OwnableManager.java
@@ -1,6 +1,16 @@
 package edu.brown.cs.cmen.brownopoly.ownable;
 
+import java.util.Map;
+
 public class OwnableManager {
+
+  private static final OwnableFactory factory = new OwnableFactory();
+  private static final Map<Integer, Property> properties = factory
+      .buildProperties();
+  private static final Map<Integer, Railroad> railroads = factory
+      .buildRailroads();
+  private static final Map<Integer, Property> utilities = factory
+      .buildUtilities();
 
   public static Property getProperty(int i) {
     return null;
