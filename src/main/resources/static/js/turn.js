ccedee7a8e5743fabe8e286f49221a1aa906d309
Nickie Pucel <nicholas_pucel@brown.edu>
2015-04-30 01:07:02 -0400

    made scrolling through news feed cleaner

--- src/main/resources/static/js/turn.js
+++ src/main/resources/static/js/turn.js
@@ -1,4 +1,5 @@
-function scrollNewsfeed() {
+function scrollNewsfeed(toAppend) {
+	newsfeed.append(toAppend);
 	var tempNewsfeed = document.getElementById("newsfeed");
 	tempNewsfeed.scrollTop = tempNewsfeed.scrollHeight;
 }
@@ -8,9 +9,7 @@ function getOutOfJail(jailCard) {
 	$.post("/getOutOfJail", params, function(responseJSON) {
 		currPlayer = JSON.parse(responseJSON).player;
 		loadPlayer(currPlayer);
-		newsfeed.append("-> " + currPlayer.name + " is out of Jail!\n");
-		//newsfeed.scrollTop = newsfeed.scrollHeight;
-		scrollNewsfeed();
+		scrollNewsfeed("-> " + currPlayer.name + " is out of Jail!\n");
 	});
 }
 
@@ -31,19 +30,12 @@ function startTurn() {
 			if (prevPlayer != null) {
 				loadPlayer(prevPlayer);
 				if (prevPlayer.id == currPlayer.id) {
-					newsfeed.append(currPlayer.name + " rolled doubles. Roll again!\n");
-					//newsfeed.scrollTop = newsfeed.scrollHeight;
-					scrollNewsfeed();
-
+					scrollNewsfeed(currPlayer.name + " rolled doubles. Roll again!\n");
 				} else {
-					newsfeed.append("It is " + currPlayer.name + "'s turn!\n");
-					//newsfeed.scrollTop = newsfeed.scrollHeight;
-					scrollNewsfeed();
+					scrollNewsfeed.append("It is " + currPlayer.name + "'s turn!\n");
 				}
 			} else {
-				newsfeed.append("It is " + currPlayer.name + "'s turn! Roll the dice or manage/trade your properties.\n");
-				//newsfeed.scrollTop = newsfeed.scrollHeight;
-				scrollNewsfeed();
+				scrollNewsfeed("It is " + currPlayer.name + "'s turn! Roll the dice or manage/trade your properties.\n");
 			}
 
 			prevPlayer = currPlayer;
@@ -63,10 +55,8 @@ function startTurn() {
 							getOutOfJail(0);
 						}
 					} else {
-						newsfeed.append("-> You are in jail. You can try to roll doubles, " +
+						scrollNewsfeed("-> You are in jail. You can try to roll doubles, " +
 							"pay $50 or use a Get Out of Jail Free card.\n");
-							//newsfeed.scrollTop = newsfeed.scrollHeight;
-							scrollNewsfeed();
 						if (currPlayer.jailFree) {
 							if (confirm("You have a Get Out of Jail Free card! Do you want to use it?")) {
 								getOutOfJail(1);
@@ -84,9 +74,7 @@ function startTurn() {
 				}
 			} else {
 				if (currPlayer.exitedJail) {
-					newsfeed.append("-> " + currPlayer.name + " is out of jail.\n");
-					//newsfeed.scrollTop = newsfeed.scrollHeight;
-					scrollNewsfeed();
+					scrollNewsfeed("-> " + currPlayer.name + " is out of jail.\n");
 				}
 				$.post("/startAITurn", function(responseJSON) {
 					var responseObject = JSON.parse(responseJSON);
@@ -95,9 +83,7 @@ function startTurn() {
 					currPlayer = responseObject.AI;
 					if (build != "") {
 						loadPlayer(currPlayer);
-						newsfeed.append("-> " + build + "\n");
-						//newsfeed.scrollTop = newsfeed.scrollHeight;
-						scrollNewsfeed();
+						scrollNewsfeed("-> " + build + "\n");
 					}
 					if (trade.hasTrade) {
 						proposeTrade(trade);
@@ -131,22 +117,16 @@ function proposeTrade(trade) {
 			}
 		}, 200);
 	} else {
-		newsfeed.append("-> " + currPlayer.name + " proposed a trade to " + recipient.name + "!\n");
-		//newsfeed.scrollTop = newsfeed.scrollHeight;
-		scrollNewsfeed();
+		scrollNewsfeed("-> " + currPlayer.name + " proposed a trade to " + recipient.name + "!\n");
 		var postParameters = {recipient: recipient.id, initProps: JSON.stringify(trade.initProps), initMoney: trade.initMoney,
 			recipProps: JSON.stringify(trade.recipProps), recipMoney: trade.recipMoney};
 		$.post("/trade", postParameters, function(responseJSON){
 			var responseObject = JSON.parse(responseJSON);
 			currPlayer = responseObject.initiator;
 			if (responseObject.accepted) {
-				newsfeed.append("-> " + recipient.name + " accepted the trade!\n");
-				//newsfeed.scrollTop = newsfeed.scrollHeight;
-				scrollNewsfeed();
+				scrollNewsfeed("-> " + recipient.name + " accepted the trade!\n");
 			} else {
-				newsfeed.append("-> " + recipient.name + " rejected the trade!\n");
-				//newsfeed.scrollTop = newsfeed.scrollHeight;
-				scrollNewsfeed();
+				scrollNewsfeed.append("-> " + recipient.name + " rejected the trade!\n");
 			}
 			roll();
 		});
@@ -160,24 +140,16 @@ function roll() {
 		var dice = result.dice;
 	    var canMove = result.canMove;
 
-		newsfeed.append("-> " + currPlayer.name + " rolled a " + dice.die1.num + " and a " + dice.die2.num + "\n");
-		//newsfeed.scrollTop = newsfeed.scrollHeight;
-		scrollNewsfeed();
+		scrollNewsfeed("-> " + currPlayer.name + " rolled a " + dice.die1.num + " and a " + dice.die2.num + "\n");
 		if (currPlayer.inJail && canMove) {
-			newsfeed.append("-> player is out of Jail!\n");
-			//newsfeed.scrollTop = newsfeed.scrollHeight;
-			scrollNewsfeed();
+			scrollNewsfeed("-> player is out of Jail!\n");
 			move(dice.die1.num + dice.die2.num);
 		} else if (!currPlayer.inJail && !canMove) {
-			newsfeed.append("-> rolled doubles 3 times, sent to Jail!\n");
-			//newsfeed.scrollTop = newsfeed.scrollHeight;
-			scrollNewsfeed();
+			scrollNewsfeed("-> rolled doubles 3 times, sent to Jail!\n");
 			secondMove = true;
 			move((10 - prevPosition + 40) % 40);
 		} else if (currPlayer.inJail && !canMove) {
-			newsfeed.append("-> Still in jail.\n");
-			//newsfeed.scrollTop = newsfeed.scrollHeight;
-			scrollNewsfeed();
+			scrollNewsfeed("-> Still in jail.\n");
 			startTurn();
 		} else if (!currPlayer.inJail && canMove) {
 			move(dice.die1.num + dice.die2.num);
@@ -198,17 +170,13 @@ function move(dist) {
 
 	//wait until all animations are finished
 	$("img").filter(":animated").promise().done(function() {
-		console.log("animations done");
 		$.post("/move", postParameters, function (responseJSON) {
 			var result = JSON.parse(responseJSON);
 			var squareName = result.squareName;
 			var inputNeeded = result.inputNeeded;
-			console.log("player pos: " + result.player.position);
 			prevPosition = result.player.position;
 			if (!secondMove) {
-				newsfeed.append("-> " + currPlayer.name + " landed on " + squareName + "!\n");
-				//newsfeed.scrollTop = newsfeed.scrollHeight;
-				scrollNewsfeed();
+				scrollNewsfeed("-> " + currPlayer.name + " landed on " + squareName + "!\n");
 			}
 			secondMove = false;
 			if (currPlayer.isAI) {
@@ -235,13 +203,10 @@ function execute(inputNeeded) {
 		var result = JSON.parse(responseJSON);
 		currPlayer = result.player;
 		if (result.message != "") {
-			newsfeed.append("-> " + result.message + "\n");
-			newsfeed.append("-> " + currPlayer.name + " has a balance of $" + currPlayer.balance + "\n");
-			//newsfeed.scrollTop = newsfeed.scrollHeight;
-			scrollNewsfeed();
+			scrollNewsfeed("-> " + result.message + "\n");
+			scrollNewsfeed("-> " + currPlayer.name + " has a balance of $" + currPlayer.balance + "\n");
 		}
 		
-		//console.log("prev: " + prevPosition + ", curr: " + currPlayer.position);
 		if (prevPosition != currPlayer.position) {
 			secondMove = true;
 			move((currPlayer.position - prevPosition + 40) % 40);
