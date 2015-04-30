7d154e137c8126199ed01559061e1e0ad3d5b692
Cody Yu <cody_yu@brown.edu>
2015-04-30 03:17:13 -0400

    get out of jail free instantiated to 0

--- src/main/java/edu/brown/cs/cmen/brownopoly/player/Player.java
+++ src/main/java/edu/brown/cs/cmen/brownopoly/player/Player.java
@@ -43,6 +43,7 @@ public abstract class Player implements Serializable {
     this.lastPosition = 0;
     this.exitedJail = false;
     this.opponents = new ArrayList<>();
+    this.getOutOfJailFree = 0;
   }
 
   public boolean isAI() {
